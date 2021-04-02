1a5bd8c8b976ac44b4c88db0868c991c473a0da2
Seunghwanly <seunghwanly@gmail.com>
2021-04-02 18:37:35 +0900

    2021/04/02 upload

---
+++ LEVEL2/피보나치수/index.js
@@ -0,0 +1,48 @@
+const fibonacci = (n) => {
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}   // very very inefficient : O(2^n)
+
+const memoizationFibonacci = (n, memo = []) => {
+    let result = 0;
+    if (memo[n] !== undefined) {
+        return memo[n];
+    }
+    if (n <= 1) {
+        result = n;
+    }
+    else {
+        result = memoizationFibonacci(n - 1, memo) + memoizationFibonacci(n - 2, memo);
+    }
+    memo[n] = result;
+    return result;
+}   // O(2n + 1)
+
+const bottomupFibonacci = (n) => {
+    if (n <= 1) {
+        return n;
+    }
+    let bottomupList = [];
+    bottomupList[0] = 0;
+    bottomupList[1] = 1;
+    // for loop to n
+    for (let i = 2; i <= n + 1; ++i) {
+        bottomupList[i] = bottomupList[i - 1] % 1234567 + bottomupList[i - 2] % 1234567;
+    }
+    return bottomupList[n];
+}   // O(n) : travel only once
+
+function solution(n) {
+    var answer = 0;
+
+    // answer = fibonacci(n) % 1234567;
+    // answer = memoizationFibonacci(n) % 1234567;
+    answer = bottomupFibonacci(n) % 1234567;
+
+    return answer;
+}
+
+console.log(solution(3));
+console.log(solution(5));
