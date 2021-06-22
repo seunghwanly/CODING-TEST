4acd6c889b4efdacc3187c2c089167e1907420f5
Seunghwanly <seunghwanly@gmail.com>
2021-06-22 23:59:29 +0900

    2021/06/22 upload

---
+++ LEVEL2/게임맵최단거리/solution.py
@@ -0,0 +1,54 @@
+def inRange(y, x, N, M):
+    return (0 <= y and y < N) and (0 <= x and x < M)
+
+
+def isSurrounded(N, M, maps):
+    cnt = 0
+    dy = [-1, -1, 0]
+    dx = [0, -1, -1]
+
+    for i in range(3):
+        nextY = N + dy[i]
+        nextX = M + dx[i]
+        if maps[nextY][nextX] == 0:
+            cnt += 1
+
+    return True if cnt == 3 else False
+
+
+def solution(maps):
+    answer = 0
+    N, M = len(maps), len(maps[0])
+    visited = [[False for _ in range(M)] for _ in range(N)]
+
+    # direction
+    dy = [0, 0, 1, -1]
+    dx = [1, -1, 0, 0]
+    print(N, M)
+    if isSurrounded(N - 1, M - 1, maps): return -1
+    # init
+    que = []
+    block = 1
+    visited[0][0] = True
+    que.append((0, 0, block))
+
+    while que:
+        front = que.pop(0)
+
+        if front[0] == (N - 1) and front[1] == (M - 1):
+            answer = front[2]
+            break
+
+        for i in range(4):
+            nextY = front[0] + dy[i]
+            nextX = front[1] + dx[i]
+            if inRange(nextY, nextX, N, M) and visited[nextY][nextX] == False and maps[nextY][nextX] == 1:
+                que.append((nextY, nextX, front[2] + 1))
+                visited[nextY][nextX] = True
+
+    return answer
+
+
+print(
+    solution([[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1],
+              [1, 1, 1, 0, 1], [0, 0, 0, 0, 1]]))
